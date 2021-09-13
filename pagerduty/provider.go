3153c1a0ba7f10c5e365fc63a94ac5477aae4d4a
Jean-Baptiste Favre <jean-baptiste.favre@blablacar.com>
2021-09-13 11:34:04 +0200

    Update provider options schema

--- pagerduty/provider.go
+++ pagerduty/provider.go
@@ -26,16 +26,10 @@ func Provider() terraform.ResourceProvider {
 				DefaultFunc: schema.EnvDefaultFunc("PAGERDUTY_TOKEN", nil),
 			},
 
-			"apiurl": {
+			"service_region": {
 				Type:     schema.TypeString,
 				Optional: true,
-				Default:  "https://api.pagerduty.com",
-			},
-
-			"appurl": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "https://app.pagerduty.com",
+				Default:  "",
 			},
 		},
 
